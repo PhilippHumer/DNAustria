ae8c3037fc58dd83338ad5144c9bd3b13c17155c
GadringaStyle <ortner.markus.02@gmail.com>
2026-02-28 10:03:56 +0100

    added serilog + scalar, migrated to .NET 10, added cors + service configuration

---
+++ product/backend/DNAustria/DNAustria.Api/BuilderExtensions/SerilogExtensions.cs
@@ -0,0 +1,21 @@
+﻿
+using Serilog;
+
+namespace DNAustria.Api.BuilderExtensions;
+
+public static class SerilogExtensions
+{
+    public static WebApplicationBuilder ActivateSerilog(this WebApplicationBuilder builder)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.Console()
+            .WriteTo.File("logs/log.txt",  rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+        builder.Host.UseSerilog();
+        builder.Logging.ClearProviders();
+        builder.Logging.AddConsole();
+        builder.Logging.AddDebug();
+        return builder;
+    }
+}
---
+++ product/backend/DNAustria/DNAustria.Api/BuilderExtensions/ServiceExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace DNAustria.Api.BuilderExtensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services)
+    {
+        //TODO: register your services here...
+        return services;
+    }
+}
