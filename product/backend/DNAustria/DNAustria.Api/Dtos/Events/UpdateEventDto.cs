1437777f52246b9c54534b4fae76294b839d7d59
Andreas Kastenhofer <andreas.kastenhofer1@gmail.com>
2026-03-07 13:30:55 +0100

    changed Event Dtos and Logic

--- product/backend/DNAustria/DNAustria.Api/Dtos/Events/UpdateEventDto.cs
+++ product/backend/DNAustria/DNAustria.Api/Dtos/Events/UpdateEventDto.cs
@@ -1,23 +1,24 @@
 ﻿namespace DNAustria.Api.Dtos;
 
-public record UpdateEventDto(
-    string Name,
-    string Description,
-    string Link,
-    DateTime StartDate,
-    DateTime EndDate,
-    int Classification,
-    int Status,
-    bool HasFees,
-    bool IsOnline,
-    int? Organization,
-    string ProgramName,
-    string Format,
-    bool SchoolBookable,
-    int AgeMinimum,
-    int AgeMaximum,
-    int? Location,
-    int? Contact,
-    List<int>? TargetAudiences,
-    List<int>? Topics
-);
+public record UpdateEventDto
+{
+    public required string Name { get; init; }
+    public required string Description { get; init; }
+    public required string Link { get; init; }
+    public required DateTime StartDate { get; init; }
+    public required DateTime EndDate { get; init; }
+    public required int Classification { get; init; }
+    public required int Status { get; init; }
+    public required bool HasFees { get; init; }
+    public required bool IsOnline { get; init; }
+    public required int? Organization { get; init; }
+    public required string ProgramName { get; init; }
+    public required string Format { get; init; }
+    public required bool SchoolBookable { get; init; }
+    public required int AgeMinimum { get; init; }
+    public required int AgeMaximum { get; init; }
+    public required int? Location { get; init; }
+    public required int? Contact { get; init; }
+    public List<int>? TargetAudiences { get; init; }
+    public List<int>? Topics { get; init; }
+}
