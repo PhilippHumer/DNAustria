9929bbd15e7cdcdce6f0ea8af14daa4cbd43afee
Philipp Humer <philipp1.humer@gmx.at>
2026-03-07 13:54:53 +0100

    Fix organization endpoints

--- product/backend/DNAustria/DNAustria.Logic/Organizations/OrganizationsLogic.cs
+++ product/backend/DNAustria/DNAustria.Logic/Organizations/OrganizationsLogic.cs
@@ -9,19 +9,24 @@ public class OrganizationsLogic(AppDbContext db) : IOrganizationsLogic
 {
     public IEnumerable<Organization> GetAllOrganizations()
     {
-        return db.Organizations.Select(x => x.ToDomain());
+        return db.Organizations
+            .Include(x => x.AdressNavigation)
+            .Where(x => !x.IsDeleted)
+            .Select(x => x.ToDomain());
     }
 
     public IEnumerable<Organization> GetOrganizationsByName(string name)
     {
         return db.Organizations
-            .Where(x => x.Name.Contains(name))
+            .Include(x => x.AdressNavigation)
+            .Where(x => x.Name.Contains(name) && !x.IsDeleted)
             .Select(x => x.ToDomain());
     }
 
     public Organization? GetOrganizationById(int id)
     {
         return db.Organizations
+            .Include(x => x.AdressNavigation)
             .Where(x => x.Id == id)
             .Select(x => x.ToDomain())
             .FirstOrDefault();
@@ -30,22 +35,46 @@ public class OrganizationsLogic(AppDbContext db) : IOrganizationsLogic
     public async Task<bool> DeleteOrganization(int id)
     {
         var orgToDelete = db.Organizations.FirstOrDefault(x => x.Id == id);
-        if (orgToDelete != null) 
-            db.Organizations.Remove(orgToDelete);
+        
+        if (orgToDelete == null) 
+            return false;
+        
+        orgToDelete.IsDeleted = true;
+        db.Organizations.Update(orgToDelete);
         return await db.SaveChangesAsync() > 0;
+
     }
 
-    public async Task<Organization> AddOrganization(Organization organization)
+    public async Task<(Organization? organization, string msg)> AddOrganization(Organization organization)
     {
-        var addedOrg = db.Organizations.Add(organization.ToEntity());
+        if (db.Organizations.Any(org => org.Name == organization.Name))
+        {
+            return (null, "Organization already exists");
+        }
+        
+        var addedAddress = db.Addresses.Add(organization.Adress.ToEntity());
+        var dalOrg = organization.ToEntity();
+        dalOrg.AdressNavigation = addedAddress.Entity;
+        var addedOrg = db.Organizations.Add(dalOrg);
         await db.SaveChangesAsync();
-        return addedOrg.Entity.ToDomain();
+        
+        return (addedOrg.Entity.ToDomain(), string.Empty);
     }
 
-    public async Task<Organization> UpdateOrganization(Organization organization)
+    public async Task<(Organization? organization, string msg)> UpdateOrganization(Organization organization)
     {
-        var updatedOrg = db.Organizations.Update(organization.ToEntity());
+        var orgDal = db.Organizations
+            .Include(x => x.AdressNavigation)
+            .FirstOrDefault(x => x.Id == organization.Id);
+
+        if (orgDal == null)
+        {
+            return (null, "Organization not found");
+        }
+        
+        orgDal.Name = organization.Name;
+        var updatedOrg = db.Organizations.Update(orgDal);
         await db.SaveChangesAsync();
-        return updatedOrg.Entity.ToDomain();
+        return (updatedOrg.Entity.ToDomain(), string.Empty);
     }
 }
