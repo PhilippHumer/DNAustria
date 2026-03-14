260d888d73b8a077cf62e90a3805691341e9de2f
Philipp Humer <philipp1.humer@gmx.at>
2026-03-14 13:09:11 +0100

    Initial frontend add and view organization

--- product/frontend/DNAustria/src/app/organizations/organizationcard/organizationcard.ts
+++ product/frontend/DNAustria/src/app/organizations/organizationcard/organizationcard.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
+import {OrganizationDto} from '../../api';
 
 @Component({
   selector: 'app-organizationcard',
@@ -8,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class Organizationcard {
 
+  organization = input<OrganizationDto>();
 }
--- product/frontend/DNAustria/src/app/organizations/organizations.ts
+++ product/frontend/DNAustria/src/app/organizations/organizations.ts
@@ -1,12 +1,94 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, inject, OnInit, signal, ViewChild} from '@angular/core';
 import { Organizationcard } from "./organizationcard/organizationcard";
-
+import {FormsModule, NgForm} from '@angular/forms';
+import {AddressDto, CreateOrganizationDto, OrganizationDto, OrganizationsService} from '../api';
+import { Modal } from 'bootstrap';
 @Component({
   selector: 'app-organizations',
-  imports: [Organizationcard],
+  imports: [Organizationcard, FormsModule],
   templateUrl: './organizations.html',
   styleUrl: './organizations.css',
 })
-export class Organizations {
+export class Organizations implements OnInit {
 
+  organizations = signal<OrganizationDto[]>([]);
+  organizationService = inject(OrganizationsService);
+
+  @ViewChild('addOrganizationModal')
+  modalElement!: ElementRef<HTMLDivElement>;
+
+  organization: CreateOrganizationDto = {
+    name: '',
+    address: {
+      street: '',
+      zip: '',
+      city: '',
+      state: '',
+    }
+  };
+
+  onSubmit(form: NgForm): void {
+    if (form.invalid) {
+      form.control.markAllAsTouched();
+      return;
+    }
+
+    console.log('Organization submitted:', this.organization);
+
+    this.organizationService.apiOrganizationsPost(
+      this.organization).subscribe(
+      () => {
+        this.loadOrganizations();
+
+
+
+        const modalEl = this.modalElement.nativeElement;
+        const modal = Modal.getInstance(modalEl);
+
+        if (modal) {
+          modalEl.addEventListener(
+            'hidden.bs.modal',
+            () => {
+              form.resetForm({
+                name: '',
+                address: {
+                  street: '',
+                  zip: '',
+                  city: '',
+                  state: '',
+                }
+              });
+
+              document.body.classList.remove('modal-open');
+              document.body.style.removeProperty('padding-right');
+
+              document
+                .querySelectorAll('.modal-backdrop')
+                .forEach(backdrop => backdrop.remove());
+            },
+            { once: true }
+          );
+
+          modal.hide();
+        }
+      }
+    );
+
+
+    form.resetForm({
+      name: '',
+      street: '',
+      zip: '',
+      city: '',
+    });
+  }
+
+  loadOrganizations(): void {
+    this.organizationService.apiOrganizationsGet()
+      .subscribe(orgs => this.organizations.set(orgs));
+  }
+
+  ngOnInit(): void {
+    this.loadOrganizations();
+  }
 }
