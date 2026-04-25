d7437d07101d6f0358c13d5926a034f160ce0bb3
Philipp Humer <philipp1.humer@gmx.at>
2026-04-25 13:21:29 +0200

    fix remove logic for contacts

--- product/backend/DNAustria/DNAustria.Logic/ContactsLogic.cs
+++ product/backend/DNAustria/DNAustria.Logic/ContactsLogic.cs
@@ -45,7 +45,8 @@ public class ContactsLogic(AppDbContext context) : IContactsLogic
         var existing = _contacts.Find(id);
         if (existing is null) throw new NotFoundException($"Contact with id {id} not found");
         
-        _contacts.Remove(existing);
+        existing.IsDeleted = true;
+        _contacts.Update(existing);
         return context.SaveChangesAsync();
     }
 }
